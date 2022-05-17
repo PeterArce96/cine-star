15e0fb11d8554abe36eb3743411b9641c1a9a1a0
PeterArce96 <parce0496@gmail.com>
2022-05-16 22:15:04 -0500

    instalacion axios

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,22 +1,33 @@
 import '../styles/css/Movies.css';
 import '../components/movies/MoviesCard';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const Movies = () => {
     useEffect(() => {
         const fetchApi = async () => {
             const endpoint = `${process.env.REACT_APP_URL}/discover/movie`;
+            // const options = {
+            //     method: 'GET',
+            //     headers: {
+            //         'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+            //         'Content-Type': 'application/json;charset=utf-8'
+            //     }
+            // };
             const options = {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
                     'Content-Type': 'application/json;charset=utf-8'
-                }
+                },
+                url: endpoint
             };
             try {
-                const response = await fetch(endpoint, options);
-                const data = await response.json();
-                console.log(data);
+                // const response = await fetch(endpoint, options);
+                // const data = await response.json();
+                const response = await axios(options);
+                console.log(response.data);
+                // console.log(data);
                 } catch (error) {
                 console.log(error);
             }
